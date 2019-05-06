10bdbae08e809f1490b52393fbe0776bce6d2be0
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-06 11:03:28 -0400

    Working on the getData with Alex

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,34 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore, Reducer, AnyAction } from 'redux';
 import { Provider } from 'react-redux';
-import schema from './schema/scidata_unit.json';
+//import schema from './schema/scidata_unit.json';
+import getData from './getData';
 import uischema from './uischema/scidata_unit.json';
 import { Actions, jsonformsReducer, JsonFormsState, createAjv } from '@jsonforms/core';
 import { materialCells, materialRenderers } from '@jsonforms/material-renderers';
 
+const unitSchemaURL = "https://raw.githubusercontent.com/marshallmcdonnell/scidata/unit_schema/schema/scidata_unit.json";
+getData('GET', unitSchemaURL).then(function(schema){ 
+  const data = {};
 
-const data = {};
+  initState: JsonFormsState = {
+      jsonforms: {
+        cells: materialCells,
+        renderers: materialRenderers
+      }
+  }
 
-const initState: JsonFormsState = {
-    jsonforms: {
-      cells: materialCells,
-      renderers: materialRenderers
-    }
-}
+  const rootReducer: Reducer<JsonFormsState, AnyAction> = combineReducers({ jsonforms: jsonformsReducer() });
+  const store = createStore(rootReducer, initState);
 
-const rootReducer: Reducer<JsonFormsState, AnyAction> = combineReducers({ jsonforms: jsonformsReducer() });
-const store = createStore(rootReducer, initState);
+  const ajv = createAjv({
+      useDefaults: true
+  })
 
-const ajv = createAjv({
-    useDefaults: true
-})
+  store.dispatch(Actions.init(data, schema, uischema, ajv));
+  return store;
+});
 
-store.dispatch(Actions.init(data, schema, uischema, ajv));
 
 ReactDOM.render(
   <Provider store={store}>
