d3269e005fcb464588688d3d32c457e6b4b9ddd4
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-17 10:08:42 -0400

    Added resolvers for combining Unit and Value schemas

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { combineReducers, createStore, Reducer, AnyAction } from 'redux';
 import { Provider } from 'react-redux';
-import schema from './schema/scidata_value.json';
-import uischema from './uischema/scidata_value.json';
 import { Actions, jsonformsReducer, JsonFormsState, createAjv } from '@jsonforms/core';
 import { materialCells, materialRenderers } from '@jsonforms/material-renderers';
+
+
+import contextSchema from './schema/scidata_context.json';
+import unitSchema from './schema/scidata_unit.json';
+import valueSchema from './schema/scidata_value.json';
+import uischema from './uischema/scidata_value.json';
 
 const data = {};
 
@@ -17,16 +21,66 @@ const initState: JsonFormsState = {
       cells: materialCells,
       renderers: materialRenderers
     }
-}
-
-const rootReducer: Reducer<JsonFormsState, AnyAction> = combineReducers({ jsonforms: jsonformsReducer() });
-const store = createStore(rootReducer, initState);
+};
 
 const ajv = createAjv({
     useDefaults: true
 })
 
-store.dispatch(Actions.init(data, schema, uischema, ajv));
+
+const contextSchemaIdentifier = 'scidata_context.json';
+const unitSchemaIdentifier = 'scidata_unit.json';
+
+ajv.addSchema(contextSchema, contextSchemaIdentifier);
+ajv.addSchema(unitSchema, unitSchemaIdentifier);
+
+
+const rootReducer: Reducer<JsonFormsState, AnyAction> = combineReducers({ jsonforms: jsonformsReducer() });
+const store = createStore(rootReducer, initState);
+
+
+// Allow json-schema-ref-resolver to resolve same schema
+const contextSchemaResolver = {
+  order: 1,
+  canRead: function(file: AnyAction) {
+      return file.url.indexOf(contextSchemaIdentifier) !== -1;
+  },
+  read: function() {
+      return JSON.stringify(contextSchema)
+  }
+}
+
+const unitSchemaResolver = {
+  order: 1,
+  canRead: function(file: AnyAction) {
+      return file.url.indexOf(unitSchemaIdentifier) !== -1;
+  },
+  read: function() {
+      return JSON.stringify(unitSchema)
+  }
+}
+
+// configuration object for JSONForms
+const jsonFormsConfiguration = {
+  ajv: ajv,
+  refParserOptions: {
+    resolve: {
+      foo: contextSchemaResolver,
+      bar: unitSchemaResolver
+    } as any
+  }
+}
+
+// Add configuration to JSONForms
+store.dispatch(
+  Actions.init(
+    data,
+    valueSchema,
+    uischema,
+    jsonFormsConfiguration
+  )
+);
+
 
 ReactDOM.render(
   <Provider store={store}>
