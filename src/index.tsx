d7b8638d6d7d57a66b1400b790ec7f364dc24c41
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-21 08:42:30 -0400

    Cleaned up main imports and update .gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,11 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Actions, JsonFormsState, createAjv } from '@jsonforms/core';
+import { Actions, JsonFormsState } from '@jsonforms/core';
 import { materialCells, materialRenderers } from '@jsonforms/material-renderers';
 
-
-import contextSchema from './state/ducks/scidata/schemas/context.json';
-import unitSchema from './state/ducks/scidata/schemas/unit.json';
 import jsonSchema from './state/ducks/scidata/jsonSchema.json';
 import uischema from './state/ducks/scidata/uiSchema.json';
-
-import { contextSchemaIdentifier, contextSchemaResolver } from './state/ducks/scidata/context/resolver'
-import { unitSchemaIdentifier, unitSchemaResolver } from './state/ducks/scidata/unit/resolver'
 
 import { default as scidataJsonFormsConfiguration } from './state/ducks/scidata/config'
 
