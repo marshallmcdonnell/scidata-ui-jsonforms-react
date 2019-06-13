3ebafe1e9683682a659ebbe6ceee3a8f164581c3
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-06-13 15:13:00 -0400

    Bug fix to schema resolvers

--- src/state/ducks/scidata/config.js
+++ src/state/ducks/scidata/config.js
@@ -6,20 +6,24 @@ function generateSchemaResolver(name, schema) {
   return {
     order: 1,
     canRead: function(file) {
+        console.log("CanRead:", file.url, name,file.url.indexOf(name + '.json') !== -1 )
         return file.url.indexOf(name + '.json') !== -1;
     },
     read: function() {
+        console.log("Read")
         return JSON.stringify(schema)
     }
   }
 }
 
 // Combined schema validator and schema resolver
+console.log('SchemaList:', schemaList)
 const ajv = createAjv({useDefaults: true});
 var resolvers = {'context': generateSchemaResolver('context', schemas['context'])};
 for( var i = 0; i < schemaList.length; i++) {
   const name = schemaList[i];
   ajv.addSchema(schemas[name], name + '.json');
+  console.log('schema name:', generateSchemaResolver(name, schemas[name]))
   resolvers[name] = generateSchemaResolver(name, schemas[name]);
 }
 
@@ -29,7 +33,7 @@ console.log("resolvers:", resolvers)
 const jsonFormsConfiguration = {
   ajv: ajv,
   refParserOptions: {
-    resolve: { resolvers }
+    resolve: resolvers
   }
 };
 
