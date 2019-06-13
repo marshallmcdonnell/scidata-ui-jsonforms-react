4de78172622adc0f0260e22c4ea692223efc37ca
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-06-13 15:13:42 -0400

    Removed debug lines in config.js

--- src/state/ducks/scidata/config.js
+++ src/state/ducks/scidata/config.js
@@ -6,11 +6,9 @@ function generateSchemaResolver(name, schema) {
   return {
     order: 1,
     canRead: function(file) {
-        console.log("CanRead:", file.url, name,file.url.indexOf(name + '.json') !== -1 )
         return file.url.indexOf(name + '.json') !== -1;
     },
     read: function() {
-        console.log("Read")
         return JSON.stringify(schema)
     }
   }
@@ -23,11 +21,8 @@ var resolvers = {'context': generateSchemaResolver('context', schemas['context']
 for( var i = 0; i < schemaList.length; i++) {
   const name = schemaList[i];
   ajv.addSchema(schemas[name], name + '.json');
-  console.log('schema name:', generateSchemaResolver(name, schemas[name]))
   resolvers[name] = generateSchemaResolver(name, schemas[name]);
 }
-
-console.log("resolvers:", resolvers)
 
 // JSONForms configuration for Redux Store
 const jsonFormsConfiguration = {
