3747ec0ec4b1442851b4f31679750752b89bc428
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-06-17 12:45:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/scidata-ui-jsonforms-react/precache-manifest.a8dc2300dbdfacc21ea6b0ecc23606d3.js"
+  "/scidata-ui-jsonforms-react/precache-manifest.575ad27edc0769e39091694a27c0db95.js"
 );
 
 workbox.clientsClaim();
