f1cc009ffbab250e4081a5fb8348d9ec2dab6400
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-06-18 10:26:05 -0400

    Fixes to resource ui schema

--- src/state/ducks/scidata/uischema/index.tsx
+++ src/state/ducks/scidata/uischema/index.tsx
@@ -1,4 +1,5 @@
 import datasetUiSchema from './dataset.json';
+import resourceUiSchema from './resource.json';
 import parameterUiSchema from './parameter.json';
 import unitUiSchema from './unit.json';
 import valueUiSchema from './value.json';
@@ -7,6 +8,7 @@ import valueUiSchema from './value.json';
 const uischemas = {
     dataset: datasetUiSchema,
     parameter: parameterUiSchema,
+    resource: resourceUiSchema,
     unit: unitUiSchema,
     value: valueUiSchema,
 }
